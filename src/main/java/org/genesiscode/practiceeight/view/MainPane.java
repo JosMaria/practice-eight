df6ff69b2c37e02a13b11228dc5a5b6968bbd8f3
Jose Maria <josmariaguilar@gmail.com>
2022-06-04 23:54:39 -0400

    build interface user

--- src/main/java/org/genesiscode/practiceeight/view/MainPane.java
+++ src/main/java/org/genesiscode/practiceeight/view/MainPane.java
@@ -2,11 +2,14 @@ package org.genesiscode.practiceeight.view;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import org.genesiscode.practiceeight.view.row.RowRandomNumber;
+
+import java.util.List;
 
 public class MainPane {
 
@@ -16,7 +19,8 @@ public class MainPane {
     private static final Font FONT_SUBTITLE = new Font("Monospace Bold", 14);
     private static final Font FONT_TITLE = new Font("Ubuntu Bold", 20);
 
-    private TextField fieldSalePrice, fieldCost, fieldPriceWithDiscount, fieldDecisionVariable;
+    private TextField fieldSalePrice, fieldCost, fieldPriceWithDiscount, fieldDecisionVariable, fieldRandomNumbers;
+    private TableView<RowRandomNumber> randomNumbersTable;
 
     private MainPane() {
         loadControls();
@@ -37,6 +41,25 @@ public class MainPane {
         fieldSalePrice = new TextField();
         fieldCost = new TextField();
         fieldPriceWithDiscount = new TextField();
+        buildRandomNumbersTable();
+    }
+
+    private void buildRandomNumbersTable() {
+        randomNumbersTable = new TableView<>();
+        randomNumbersTable.setMaxWidth(170);
+        randomNumbersTable.getColumns().addAll(List.of(
+                column("Fila", "rowNumber", 60),
+                column("Numero\nAleatorio", "randomNumber", 110)
+        ));
+        randomNumbersTable.setMaxHeight(300);
+    }
+
+    private VBox buildPaneInputRandomNumbers() {
+        fieldRandomNumbers = new TextField();
+        fieldRandomNumbers.setPrefColumnCount(20);
+        Button btnAdd = new Button("Agregar");
+        btnAdd.setOnAction(actionEvent -> System.out.println("Agregar"));
+        return new VBox(10, new Label("Numeros Aleatorios"), fieldRandomNumbers, btnAdd);
     }
 
     private VBox decisionVariablePane() {
@@ -64,11 +87,21 @@ public class MainPane {
     }
 
     public void buildPane() {
-        Label title = new Label("DULCE  ADA ");
+        Label title = new Label("DULCE  ADA");
         title.setFont(FONT_TITLE);
 
-        this.pane = new VBox(20, title, decisionVariablePane(), parametersPane());
-        this.pane.setAlignment(Pos.CENTER);
-        this.pane.setPadding(new Insets(10));
+        VBox rightPane = new VBox(10, buildPaneInputRandomNumbers(), randomNumbersTable);
+        VBox leftPane = new VBox(40, decisionVariablePane(), parametersPane());
+        leftPane.setAlignment(Pos.TOP_CENTER);
+        pane = new VBox(20, title, new HBox(20, leftPane, rightPane));
+        pane.setAlignment(Pos.CENTER);
+        pane.setPadding(new Insets(30));
+    }
+
+    private <U, T> TableColumn<U, T> column(String titleColumn, String property, double prefSize) {
+        TableColumn<U, T> column = new TableColumn<>(titleColumn);
+        column.setCellValueFactory(new PropertyValueFactory<>(property));
+        column.setPrefWidth(prefSize);
+        return column;
     }
 }
